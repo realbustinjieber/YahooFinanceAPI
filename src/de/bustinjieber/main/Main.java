3e343482f933572a34c586b98082808175d7e79e
realbustinjieber <jandoesgr8@gmail.com>
2026-04-29 12:12:15 +0200

    added Scraping for some of the statisticals below the chart

--- src/de/bustinjieber/main/Main.java
+++ src/de/bustinjieber/main/Main.java
@@ -4,14 +4,21 @@ import de.bustinjieber.main.yahoo.Ticker;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class Main {
+
+    public Float calcMarketCap(String mC){
+        if(mC.contains("B")){
+            return Float.parseFloat(mC.replace("B", "")) * 1000000000000f;
+        }
+        return 0f;
+    }
+
     // Beispiel: https://finance.yahoo.com/quote/AAPL
     public static void main(String[] args) throws IOException {
-        String ticker = "^GSPC"; // anpassen
+        String ticker = "UPS"; // anpassen
         String url = "https://finance.yahoo.com/quote/" + ticker;
 
         // Setze user-agent und optional Timeout
@@ -25,8 +32,13 @@ public class Main {
         Element changeElem = doc.selectFirst("[data-testid=qsp-price-change]");
         Element changePctElem = doc.selectFirst("[data-testid=qsp-price-change-percent]");
         Element quoteElem = doc.selectFirst("[data-testid=quote-title]");
-
-        System.out.println();
+        Element prevCloseElem = doc.selectFirst("[data-field=regularMarketPreviousClose]");
+        Element openElem = doc.selectFirst("[data-field=regularMarketOpen]");
+        Element daysRangeElem = doc.selectFirst("[data-field=regularMarketDayRange]");
+        Element volumeElem = doc.selectFirst("[data-field=regularMarketVolume]");
+        Element avgVolumeElem = doc.selectFirst("[data-field=averageVolume]");
+        Element fiftyTwoWeekRangeElem = doc.selectFirst("[data-field=fiftyTwoWeekRange]");
+        Element marketCapElem = doc.selectFirst("[data-field=marketCap]");
 
         Ticker t1 = new Ticker(
                 quoteElem.text().split("\\(")[1].replace("(","").replace(")",""),
@@ -35,6 +47,22 @@ public class Main {
                 Float.parseFloat(changeElem.text()),
                 Float.parseFloat(changePctElem.text().replace("%","").replace("(","").replace(")",""))
         );
+
+        t1.setPrevClose(Float.parseFloat(prevCloseElem.text().replace(",","")));
+        t1.setOpen(Float.parseFloat(openElem.text().replace(",","")));
+        t1.setDaysRange(new Float[]{
+                Float.parseFloat(daysRangeElem.text().split("-")[0].replace(",", "")),
+                Float.parseFloat(daysRangeElem.text().split("-")[1].replace(",", ""))
+        });
+        t1.setVolume(Long.parseLong(volumeElem.text().replace(",","")));
+        t1.setAvgVolume(Float.parseFloat(avgVolumeElem.text().replace(",","")));
+        t1.setFiftyTwoWeekRange(new Float[]{
+                Float.parseFloat(fiftyTwoWeekRangeElem.text().split("-")[0].replace(",", "")),
+                Float.parseFloat(fiftyTwoWeekRangeElem.text().split("-")[1].replace(",", ""))
+        });
+        Main m = new Main();
+        t1.setMarketCap(m.calcMarketCap(marketCapElem.text().replace(",","")));
+
 
         System.out.println(t1.toString());
 //
--- src/de/bustinjieber/main/yahoo/Ticker.java
+++ src/de/bustinjieber/main/yahoo/Ticker.java
@@ -1,5 +1,9 @@
 package de.bustinjieber.main.yahoo;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 /**
  * Helper Data-Class for storing data relating to a certain Ticker
  */
@@ -10,6 +14,19 @@ public class Ticker {
     private Float price;
     private Float change;
     private Float changePercentage;
+    /**
+     * statistics-content 1/2
+     */
+    private Long volume;
+    private Float avgVolume;
+    private Float[] daysRange;
+    private Float open;
+    private Float prevClose;
+    private Float[] fiftyTwoWeekRange;
+    /**
+     * statistics-content 2/2 - needs to be checked if null, as they don't always exit (example: S&P 500)
+     */
+    private Float marketCap;
 
     public Ticker(){}
 
@@ -28,9 +45,15 @@ public class Ticker {
     public String toString(){
        return "Ticker: " + getTicker() + "\n"
                + "Title: " + getQuoteTitle() + "\n"
-               + "Price: " + getPrice() + "\n"
-               + "Change: " + getChange() + "\n"
-               + "Change %: " + getChangePercentage();
+               + "Price: $ " + getPrice() + "\n"
+               + "Change: $ " + getChange() + " / % " + getChangePercentage() + "\n"
+               + "Open: $ " + getOpen() + "\n"
+               + "Previous Close: $ " + getPrevClose() + "\n"
+               + "Volume: " + getVolume() + "\n"
+               + "Avg. Volume: " + getAvgVolume() + "\n"
+               + "Market Cap: $ " + getMarketCap() + "\n"
+               + "Days Range: $ " + getDaysRange()[0] + " - $ " + getDaysRange()[1] + "\n"
+               + "52 Week Range: $ " + getFiftyTwoWeekRange()[0] + " - $ " + getFiftyTwoWeekRange()[1] + "\n";
     }
 
     public String getTicker() {
@@ -71,5 +94,61 @@ public class Ticker {
 
     public void setChangePercentage(Float changePercentage) {
         this.changePercentage = changePercentage;
+    }
+
+    public Long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Long volume) {
+        this.volume = volume;
+    }
+
+    public Float getAvgVolume() {
+        return avgVolume;
+    }
+
+    public void setAvgVolume(Float avgVolume) {
+        this.avgVolume = avgVolume;
+    }
+
+    public Float[] getDaysRange() {
+        return daysRange;
+    }
+
+    public void setDaysRange(Float[] daysRange) {
+        this.daysRange = daysRange;
+    }
+
+    public Float getOpen() {
+        return open;
+    }
+
+    public void setOpen(Float open) {
+        this.open = open;
+    }
+
+    public Float getPrevClose() {
+        return prevClose;
+    }
+
+    public void setPrevClose(Float prevClose) {
+        this.prevClose = prevClose;
+    }
+
+    public Float[] getFiftyTwoWeekRange() {
+        return fiftyTwoWeekRange;
+    }
+
+    public void setFiftyTwoWeekRange(Float[] fiftyTwoWeekRange) {
+        this.fiftyTwoWeekRange = fiftyTwoWeekRange;
+    }
+
+    public Float getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(Float marketCap) {
+        this.marketCap = marketCap;
     }
 }
